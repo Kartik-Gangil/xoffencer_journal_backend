6396e75a2e0457a4172e2bd6ec0173596cfe2fef
kartikgupta666 <kartikgangil@gmail.com>
2025-04-20 00:29:13 +0530

    fixing server fro deployment

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const cors = require("cors");
 const Form_Submission = require("./Routes/Form_Submission");
 const pool = require("./Database");
 const { config } = require("dotenv");
-const https = require('https');
-const http = require('http');
-const fs = require('fs');
+
 config();
 
 const app = express();
@@ -14,10 +12,7 @@ app.use(cors({
     origin: "*"
 }));
 
-const sslOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/varsharesearchorganization.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/varsharesearchorganization.com/fullchain.pem')
-};
+
 
 
 const PORT = process.env.PORT || 8000;
@@ -130,13 +125,6 @@ pool.query(`
 
 app.use("/api/v1", Form_Submission);
 
-https.createServer(sslOptions, app).listen(PORT, () => {
-    console.log('✅ HTTPS Server running on https://varsharesearchorganization.com');
-});
-
-http.createServer((req, res) => {
-    res.writeHead(301, { "Location": "https://" + req.headers.host + req.url });
-    res.end();
-}).listen(80, () => {
-    console.log('🌐 Redirecting HTTP traffic to HTTPS');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
